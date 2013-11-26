611e80c66ebc1a790beaf04f7cfe578ee15cc7c5
Paulo Almeida <paulo.cc03@gmail.com>
2013-11-26 14:44:00 +0700

    Endpoints API structure and connections

---
+++ src/com/narp/endpoints/Greetings.java
@@ -0,0 +1,36 @@
+package com.narp.endpoints;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import javax.inject.Named;
+import java.util.ArrayList;
+
+/**
+ * Defines v1 of a helloworld API, which provides simple "greeting" methods.
+ */
+@Api(name = "endpoint", version = "v1")
+public class Greetings {
+
+	public static ArrayList<HelloGreeting> greetings = new ArrayList<HelloGreeting>();
+
+	static {
+		greetings.add(new HelloGreeting("hello world!"));
+		greetings.add(new HelloGreeting("goodbye world!"));
+		greetings.add(new HelloGreeting("hey yo lets go....."));
+	}
+
+	public HelloGreeting getGreeting(@Named("id") Integer id) {
+		return greetings.get(id);
+	}
+
+	@ApiMethod(name = "greetings.multiply", httpMethod = "post")
+	public HelloGreeting insertGreeting(@Named("times") Integer times, HelloGreeting greeting) {
+		HelloGreeting response = new HelloGreeting();
+		StringBuilder responseBuilder = new StringBuilder();
+		for (int i = 0; i < times; i++) {
+			responseBuilder.append(greeting.getMessage());
+		}
+		response.setMessage(responseBuilder.toString());
+		return response;
+	}
+}
