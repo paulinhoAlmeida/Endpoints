9784b94b01fb3702a498cb010dc8db2fb8b3b435
Paulo Almeida <paulo.cc03@gmail.com>
2013-11-27 01:39:01 +0700

    client API key inserted I think

--- src/com/narp/endpoints/Constants.java
+++ src/com/narp/endpoints/Constants.java
@@ -4,7 +4,7 @@ package com.narp.endpoints;
  * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "replace this with your web client application ID";
+  public static final String WEB_CLIENT_ID = "454651675851.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
